3f89f8fdf74738d2f023d6d847c86b5fb6c54369
debugleader.abdallah <aboudi41444@gmail.com>
2020-10-23 10:22:26 +0400

    Updates

---
+++ precache-manifest.dff139ac4321b000d5f699ce2774d095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b52709ec419fcc45583607e1b5737df",
+    "url": "/TrackerX/index.html"
+  },
+  {
+    "revision": "16efdffa012ab0bbc6d1",
+    "url": "/TrackerX/static/css/main.7268eac7.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/TrackerX/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TrackerX/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16efdffa012ab0bbc6d1",
+    "url": "/TrackerX/static/js/main.200c6fa5.chunk.js"
+  },
+  {
+    "revision": "80e6fd71ef1bfe1903a5",
+    "url": "/TrackerX/static/js/runtime-main.8d66985e.js"
+  }
+]);
